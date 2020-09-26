daa5db24925acc62e674b6085599f534ba117f55
Dagmara <dagmarakitkowska@gmail.com>
2020-09-26 10:20:16 +0200

    first commit

---
+++ src/main/java/pl/sda/hellospring/bean/MyNewBean.java
@@ -0,0 +1,15 @@
+package pl.sda.hellospring.bean;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * ...comment class...
+ *
+ * @author dagmarakitkowska@gmail.com
+ * @since 26.09.2020
+ */
+
+@Component
+final class MyNewBean {
+
+}
